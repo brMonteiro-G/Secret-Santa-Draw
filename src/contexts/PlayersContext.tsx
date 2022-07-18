608028b57ed5cbe0839d7e229140c7994076e272
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-07-18 18:46:09 -0300

    feat():adding players list context

---
+++ src/contexts/PlayersContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
+import { Player } from "types/Player";
+
+interface IPlayerContextProps {
+  players: Player[];
+  setPlayers:Dispatch<SetStateAction<Player[]>>;
+}
+interface IPlayersProviderProps {
+  children: ReactNode;
+}
+
+export const PlayersContext = createContext<IPlayerContextProps>({
+  players: [],
+  setPlayers: () => [],
+});
+
+export const PlayersProvider = ({ children }: IPlayersProviderProps) => {
+  const [players, setPlayers] = useState<Player[]>([]);
+  return (
+    <PlayersContext.Provider value={{players,setPlayers}}> {children}</PlayersContext.Provider>
+  );
+};
