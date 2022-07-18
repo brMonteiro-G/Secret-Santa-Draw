11d9b908a6759702e3872626666610cdadc2b77e
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-07-18 00:05:12 -0300

    feat():adding Header component

---
+++ src/components/header/style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const HeaderStyle = styled.div`
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+  flex-direction: row-reverse;
+  background-color: #4b69fd;
+  display: flex;
+`;
+
+export const FigureStyle = styled.figure`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
