1c8c3340e4b8b0f81d834ad7dd028d7283ee47c3
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-07-20 18:52:07 -0300

    feat():adding form`s behaviors

--- src/components/form/index.test.tsx
+++ src/components/form/index.test.tsx
@@ -1,22 +1,55 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { PlayersContext, PlayersProvider } from "contexts/PlayersContext";
 import Form from ".";
 
 //Jest
+describe(" Component - <Form>", () => {
+  test("quando o input está vazio, novos participantes não podem ser adicionados", () => {
+    render(<Form />);
 
-test("quando o input está vazio, novos participantes não podem ser adicionados", () => {
-  render(<Form />);
+    //encontrar no DOM o input
+    const input = screen.getByPlaceholderText(
+      "Insira os nomes dos participantes"
+    );
 
-  //encontrar no DOM o input
-  const input = screen.getByPlaceholderText(
-    "Insira os nomes dos participantes"
-  );
+    //encontrar o botão
+    const botao = screen.getByRole("button");
 
-  //encontrar o botão
-  const botao = screen.getByRole("button");
+    //garantir que o input esteja no documento
+    expect(input).toBeInTheDocument();
 
-  //garantir que o input esteja no documento
-  expect(input).toBeInTheDocument();
+    //garantir que o botão esteja desabilitado
+    expect(botao).toBeDisabled();
+  });
 
-  //garantir que o botão esteja desabilitado
-  expect(botao).toBeDisabled();
+  test("quando o input for preenchido, adicionar o nome na lista ", () => {
+    render(
+      <PlayersProvider>
+        <Form />
+      </PlayersProvider>
+    );
+
+    //encontrar no DOM o input
+    const input = screen.getByPlaceholderText(
+      "Insira os nomes dos participantes"
+    );
+
+    //encontrar o botão
+    const botao = screen.getByRole("button");
+
+    //inserir um valor no input
+    fireEvent.change(input, {
+      target: "Miguel Rodrigues",
+    });
+
+    //clicar no botão de submeter
+    fireEvent.click(botao);
+
+    //garantir que o input fique vazio depois
+    expect(input).toHaveValue("");
+    
+    //garantir que o input esteja com o foco ativo, ou seja, quando o usuário terminar de clicar o cursor deve estar no input
+    //para que o usuario possa digitar apenas com o teclado + enter
+    expect(input).toHaveFocus();
+  });
 });
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,8 +1,42 @@
+import { useAddPlayer } from "hooks/addPlayer";
+import { v4 as uuidv4 } from "uuid";
+import { useRef, useState } from "react";
+import { Player } from "types/Player";
+
 const Form = () => {
+  const [player, setPlayer] = useState<Player>();
+  const addList = useAddPlayer();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const submitEvent = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    addList(player!);
+    const obj: Player = {
+      id: "",
+      name: "",
+      gift: "",
+    };
+    setPlayer(obj);
+    inputRef.current?.focus();
+  };
+
   return (
-    <form action=''>
-      <input type='text' placeholder='Insira os nomes dos participantes' />
-      <button type='submit' disabled={true}>
+    <form onSubmit={submitEvent}>
+      <input
+        type="text"
+        value={player?.name}
+        placeholder="Insira os nomes dos participantes"
+        ref={inputRef}
+        onChange={(event) => {
+          const obj: Player = {
+            id: uuidv4(),
+            name: event.target.value,
+            gift: "",
+          };
+          setPlayer(obj);
+        }}
+      />
+      <button type="submit" disabled={!player}>
         Adicionar
       </button>
     </form>
